b0e2495702d9ea831483b06519df205987746f43
Jin <zinkoko5211@gmail.com>
2024-05-01 00:54:49 +0900

    モデル単体テストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    context '商品出品できるとき' do
+      it '全ての商品が存在すれば商品出品ができる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品出品できないとき' do
+      it '商品名が空では出品できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Name can't be blank"
+      end
+      it '商品の説明が空では登録できない' do
+        @item.info = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Info can't be blank"
+      end
+      it 'カテゴリーが未選択では登録できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category can't be blank"
+      end
+      it '商品の状態が未選択では登録できない' do
+        @item.sales_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Sales status can't be blank"
+      end
+      it '配送料の負担が未選択では登録できない' do
+        @item.shipping_fee_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping fee status can't be blank"
+      end
+      it '発送元の地域が未選択では登録できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Prefecture can't be blank"
+      end
+      it '発送までの日数が未選択では登録できない' do
+        @item.schedule_delivery_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Schedule delivery can't be blank"
+      end
+      it '価格が空では登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price can't be blank"
+      end
+      it '価格は¥300より少ない商品だと出品できない' do
+        @item.price = Faker::Number.within(range: 0..299)
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'Price must be greater than or equal to 300'
+      end
+      it '価格は¥9,999,999より高い商品だと出品できない' do
+        @item.price = Faker::Number.within(range: 10_000_000..100_000_000)
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'Price must be less than or equal to 9999999'
+      end
+    end
+  end
 end
