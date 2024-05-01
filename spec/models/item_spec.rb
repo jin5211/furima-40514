491097ce973373f7cc5e41dc6ea38270bb0a2b74
Jin <zinkoko5211@gmail.com>
2024-05-01 11:40:07 +0900

    コード修正 (commision.js, item_spec.rb)

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -17,37 +17,37 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include "Name can't be blank"
       end
-      it '商品の説明が空では登録できない' do
+      it '商品の説明が空では出品できない' do
         @item.info = ''
         @item.valid?
         expect(@item.errors.full_messages).to include "Info can't be blank"
       end
-      it 'カテゴリーが未選択では登録できない' do
+      it 'カテゴリーが未選択では出品できない' do
         @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Category can't be blank"
       end
-      it '商品の状態が未選択では登録できない' do
+      it '商品の状態が未選択では出品できない' do
         @item.sales_status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Sales status can't be blank"
       end
-      it '配送料の負担が未選択では登録できない' do
+      it '配送料の負担が未選択では出品できない' do
         @item.shipping_fee_status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Shipping fee status can't be blank"
       end
-      it '発送元の地域が未選択では登録できない' do
+      it '発送元の地域が未選択では出品できない' do
         @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Prefecture can't be blank"
       end
-      it '発送までの日数が未選択では登録できない' do
+      it '発送までの日数が未選択では出品できない' do
         @item.schedule_delivery_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Schedule delivery can't be blank"
       end
-      it '価格が空では登録できない' do
+      it '価格が空では出品できない' do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include "Price can't be blank"
