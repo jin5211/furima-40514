e10a4fb18787f78202da22e41d24a8bb2c219670
Jin <zinkoko5211@gmail.com>
2024-05-02 19:44:22 +0900

    Rubocopの実行

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -68,14 +68,14 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include 'Price must be less than or equal to 9999999'
       end
       it '価格で全角入力だと出品できない' do
-        @item.price = "３００"
+        @item.price = '３００'
         @item.valid?
         expect(@item.errors.full_messages).to include 'Price is not a number'
       end
       it 'userが紐づいていないと出品できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include "User must exist"
+        expect(@item.errors.full_messages).to include 'User must exist'
       end
     end
   end
