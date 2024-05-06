a68a184b90889e46803d4477e78863803ba8f6b1
Jin <zinkoko5211@gmail.com>
2024-05-06 18:03:45 +0900

    "Update order shipping info model, adjust test environment and remove unnecessary spec files"
    This commit includes the following changes:
    
    1. In the `OrderShippingInfo` model, the validation messages for the `postal_code`

--- app/models/order_shipping_info.rb
+++ app/models/order_shipping_info.rb
@@ -6,15 +6,15 @@ class OrderShippingInfo
     validates :item_id
     validates :user_id
     validates :postal_code,
-              format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+              format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :city
     validates :address
     validates :phone_number,
-              format: { with: /\A\d{10,11}\z/, message: 'is invalid and should be 10 or 11 digits. Not include hyphen(-)' }
+              format: { with: /\A\d{10,11}\z/ }
     validates :token
     validates :price
   end
-  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates  :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
